04141f1933b27b038c0199787e0d3585b2cb6345
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2014-11-08 12:10:16 -0800

    Worked on user interface.

--- src/controller/room.js
+++ src/controller/room.js
@@ -1,7 +1,6 @@
 var exports = module.exports = {};
 
 exports.get = function (req, res) {
-  res.render('room', function(err, html) {
-    res.end(html);
-  });
+  res.render('room', {'roomId':
+     "Isaac's cool room"}); 
 };
