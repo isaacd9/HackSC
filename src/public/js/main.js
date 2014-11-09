1c2df87c230a20b215c90d0add60a50f5f2c606a
Ethan <ethan@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 19:09:21 -0800

    Added more of the AutoComplete

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,3 +1,20 @@
-var searchResult = Backbone.model.extend({
-  
+function findMatches(q, cb) {
+  $.get('/ajax/search', {'q': q}, function(data) {
+      cb(data);
+  });
+ }
+
+$(document).ready(function() {
+  $('.col-md-8 .search').typeahead(null, {
+   name: 'songs',
+   displayKey: 'value',
+   source: findMatches,
+   templates: {
+     empty: [
+	'No results found'
+     ],
+     suggestion: _.template(Name: "<%= name %>");
+   }
+  });
 });
+
