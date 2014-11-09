bdc9facc7f33c193067b3490e60ccdba580292b0
Ethan <ethan@guest-wireless-207-151-035-004.usc.edu>
2014-11-09 08:13:36 -0800

    No more transparency

--- src/models/room.js
+++ src/models/room.js
@@ -12,6 +12,12 @@ exports.getSongs = function(room, cb) {
     _.each(songs, function(song) {
       parsedJson = (JSON.parse(he.unescape(he.decode(song))));
       parsedJson = parsedJson.replace(/\n/g, "");
+      function millisToMinutes(millis) {
+        var minutes = Math.floor(millis/60000);
+        var seconds = ((millis%60000)/1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+      }
+      parsedJson.duration_ms = millisToMinutes(parsedJson.duration_ms);
       parsedSongs.push(JSON.parse(parsedJson));
     });
 
