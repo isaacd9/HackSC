d0a9edbeb84b8577ecf6aae3e1ce9727ef8c975d
Ethan <ethan@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 22:01:42 -0800

    Added tooltip

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,8 @@
 function findMatches(q, cb) {
   $.get('/ajax/search', {'q': q}, function(data) {
-      data = JSON.parse(data);
+      if (typeof data == 'string') {
+      	data = JSON.parse(data);
+      }
       cb(data.tracks.items);
   });
  }
@@ -8,14 +10,20 @@ function findMatches(q, cb) {
 $(document).ready(function() {
   $('.col-md-8 .search').typeahead(null, {
    name: 'songs',
-   displayKey: 'value',
+   displayKey: function (val) {
+	  return '<div class="item" data-id="' + val.id  + '">' + val.name + ' - <span class="artist">' + val.artists[0].name + '</span></div>'; 
+	},
    source: findMatches,
    templates: {
      empty: [
 	'No results found'
-     ],
-     suggestion: _.template("<%= value %>")
+     ]
    }
   });
+
+  $('div').on('click', function() {	
+     $('.search').val(" ");
+  });
+
 });
 
